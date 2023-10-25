ee70ca43efa41cfdbbb2835e9858eec10bc2d64a
Aaaaaaamber <xhe32778@usc.edu>
2023-10-24 23:06:08 -0700

    Add CombinedPlayer Script

---
+++ Assets/CombinedPlayer1.cs
@@ -0,0 +1,209 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CombinedPlayerController : MonoBehaviour
+{
+    // Shared Variables
+    private Rigidbody2D rb;
+
+    // Player Movement Variables
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+
+    // ResizableBox Variables
+    private SpriteRenderer sr;
+    private bool canResize = false;
+    private bool canGrowUp = true;
+    private bool canGrowDown = false;
+    private bool canGrowLeft = true;
+    private bool canGrowRight = true;
+    private bool canGrow = false;
+    private float playerMass = 1f;
+
+    // UI
+    public Text resizeHintText;
+
+    // LightBox Movement Variables
+    private bool isJumping = false;
+    private bool isCollidingWithBox = false;
+    private GameObject lightBox;
+    private bool isLeftOfBox = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        resizeHintText.enabled = false;
+        rb.mass = playerMass;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleResize();
+    }
+
+    private void HandleMovement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        if (isCollidingWithBox && isLeftOfBox && Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            lightBox.transform.position = new Vector2(lightBox.transform.position.x + moveSpeed * Time.deltaTime, lightBox.transform.position.y);
+        }
+        else if (isCollidingWithBox && !isLeftOfBox && Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            lightBox.transform.position = new Vector2(lightBox.transform.position.x - moveSpeed * Time.deltaTime, lightBox.transform.position.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = true;
+        }
+
+    }
+
+    private void HandleResize()
+    {
+        if (canResize && canGrow)
+        {
+            sr.color = Color.yellow;
+            CheckDirectionsForWalls();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                // Define the boundaries of the current object
+                float topBoundary = transform.position.y + transform.localScale.y / 2;
+                float bottomBoundary = transform.position.y - transform.localScale.y / 2;
+                float leftBoundary = transform.position.x - transform.localScale.x / 2;
+                float rightBoundary = transform.position.x + transform.localScale.x / 2;
+
+                bool hasGrown = false; // Track if player has grown
+
+                // Grow Upwards
+                if (mousePosition.y > topBoundary && canGrowUp)
+                {
+                    Debug.Log("Growing Upwards");
+                    transform.localScale += new Vector3(0, 1, 0);
+                    transform.position += new Vector3(0, 0.5f, 0);
+                    hasGrown = true;
+                }
+                // Grow Downwards
+                else if (mousePosition.y < bottomBoundary && canGrowDown)
+                {
+                    Debug.Log("Growing Downwards");
+                    transform.localScale += new Vector3(0, -1, 0);
+                    transform.position -= new Vector3(0, 0.5f, 0);
+                    hasGrown = true;
+                }
+                // Grow Left
+                else if (mousePosition.x < leftBoundary && canGrowLeft)
+                {
+                    Debug.Log("Growing Leftwards");
+                    transform.localScale += new Vector3(1, 0, 0);  // 在x轴上增加1
+                    transform.position -= new Vector3(0.5f, 0, 0);  // 位置左移0.5
+                    hasGrown = true;
+                }
+                // Grow Right
+                else if (mousePosition.x > rightBoundary && canGrowRight)
+                {
+                    Debug.Log("Growing Rightwards");
+                    transform.localScale += new Vector3(1, 0, 0);
+                    transform.position += new Vector3(0.5f, 0, 0);
+                    hasGrown = true;
+                }
+
+                // If the player has grown in any direction, reset the ability to grow
+                if (hasGrown)
+                {
+                    canGrow = false;
+                    resizeHintText.enabled = false;
+                }
+            }
+        }
+        else
+        {
+            sr.color = Color.white;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isJumping = false;
+        }
+        if (collision.gameObject.CompareTag("LightBox"))
+        {
+            isCollidingWithBox = false;
+            lightBox = null;
+        }
+    }
+
+
+    private void CheckDirectionsForWalls()
+    {
+        // 使用射线检测周围的墙壁
+        canGrowUp = !IsBlocked(Vector2.up, transform.localScale.y / 2 + 0.5f);
+        canGrowDown = false;  // Always false, because we don't want to grow downwards
+        canGrowLeft = !IsBlocked(Vector2.left, transform.localScale.x / 2 + 0.5f);
+        canGrowRight = !IsBlocked(Vector2.right, transform.localScale.x / 2 + 0.5f);
+
+    }
+
+    private bool IsBlocked(Vector2 direction, float distance)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, distance);
+        return hit.collider != null && hit.collider.tag == "Wall";
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player collided with: " + collision.gameObject.name);  
+
+        if (collision.gameObject.tag == "Gem")
+        {
+            Debug.Log("Gem detected");  
+            canResize = true;
+            canGrow = true;
+            resizeHintText.enabled = true;
+
+            playerMass *= 2;  // 玩家的质量翻倍
+            rb.mass = playerMass;
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Stone")
+        {
+            Rigidbody2D stoneRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (stoneRb)
+            {
+                // 检查玩家的质量是否大于等于石头的质量
+                if (playerMass >= stoneRb.mass)
+                {
+                    stoneRb.constraints = RigidbodyConstraints2D.None;  // 允许石头移动
+                }
+                else
+                {
+                    stoneRb.constraints = RigidbodyConstraints2D.FreezeAll;  // 冻结石头，防止玩家移动它
+                }
+            }
+        }
+    }
+
+}
