5e0a2731eb8d26341904a259aab9385e900c4565
yanjungu-usc <yanjungu@usc.edu>
2023-10-27 05:43:14 -0700

    add-tutorial-guidance-of-sound-mechanism

--- Assets/Level1 Script/SoundScreenEffect.cs
+++ Assets/Level1 Script/SoundScreenEffect.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using TMPro;
+
 
 public class SoundScreenEffect : MonoBehaviour
 {
@@ -16,10 +18,17 @@ public class SoundScreenEffect : MonoBehaviour
 
     public RectTransform canvasRect;
     public Camera mainCamera;
+    public TextMeshProUGUI leftWallText;
+    public TextMeshProUGUI rightWallText;
+
 
     public float flashDuration = 0.6f;
 
     private SoundScreenEffect soundScreenEffect;
+    private float greenDotWidth;
+    private float greenDotHeight;
+
+    private Vector3 lastPlayerPosition;
 
     private void Awake()
     {
@@ -33,11 +42,32 @@ public class SoundScreenEffect : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        if (leftWallText != null && rightWallText != null)
+        {
+            leftWallText.gameObject.SetActive(false);
+            rightWallText.gameObject.SetActive(false);
+        }
+
+        lastPlayerPosition = player.transform.position;
+    }
+
     private void Update()
     {
+        greenDotWidth = greenDot.rectTransform.rect.width;
+        greenDotHeight = greenDot.rectTransform.rect.height;
+
+        if (gem1 == null && gem2 == null && gem3 == null)
+        {
+            greenDot.enabled = false;
+        }
+
         if (gem1 != null)
         {
             Vector2 intersection = FindIntersection(player.position, gem1.position);
+            Vector3 directionToGem = gem1.position - player.position;
+            RotateArrowTowards(directionToGem, greenDot.rectTransform);
 
             //Convert the screen point to a local point within the canvas RectTransform
             Vector2 localPoint;
@@ -49,6 +79,8 @@ public class SoundScreenEffect : MonoBehaviour
         else if (gem2 != null)
         {
             Vector2 intersection = FindIntersection(player.position, gem2.position);
+            Vector3 directionToGem = gem2.position - player.position;
+            RotateArrowTowards(directionToGem, greenDot.rectTransform);
 
             //Convert the screen point to a local point within the canvas RectTransform
             Vector2 localPoint;
@@ -60,6 +92,8 @@ public class SoundScreenEffect : MonoBehaviour
         else if(gem3 != null)
         {
             Vector2 intersection = FindIntersection(player.position, gem3.position);
+            Vector3 directionToGem = gem3.position - player.position;
+            RotateArrowTowards(directionToGem, greenDot.rectTransform);
 
             //Convert the screen point to a local point within the canvas RectTransform
             Vector2 localPoint;
@@ -68,27 +102,52 @@ public class SoundScreenEffect : MonoBehaviour
                 greenDot.rectTransform.anchoredPosition = localPoint;
             }
         }
-        else
+
+        if (player.transform.position != lastPlayerPosition)
         {
-            greenDot.enabled = false;
+            Color originalColor = greenDot.color;
+            Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+            greenDot.color = flashColor;
+
+            lastPlayerPosition = player.transform.position;
+        } else
+        {
+            Color originalColor = greenDot.color;
+            Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
+            greenDot.color = flashColor;
         }
+
     }
 
 
     public void FlashLeft()
     {
-        StartCoroutine(Flash(leftFlash));
+        if (leftWallText != null && rightWallText != null)
+        {
+            StartCoroutine(FlashAndText(leftFlash, leftWallText));
+        } else
+        {
+            Debug.Log("111");
+            StartCoroutine(Flash(leftFlash));
+        }
     }
 
     public void FlashRight()
     {
-        StartCoroutine(Flash(rightFlash));
+        if (leftWallText != null && rightWallText != null)
+        {
+            StartCoroutine(FlashAndText(rightFlash, rightWallText));
+        }
+        else
+        {
+            StartCoroutine(Flash(rightFlash));
+        }
     }
 
     private IEnumerator Flash(Image img)
     {
         Color originalColor = img.color;
-        Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f); // Alpha设为1
+        Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
         img.color = flashColor;
 
         yield return new WaitForSeconds(flashDuration);
@@ -97,7 +156,31 @@ public class SoundScreenEffect : MonoBehaviour
         img.color = flashColor;
     }
 
-        Vector2 FindIntersection(Vector3 playerPos, Vector3 lightboxPos)
+    private IEnumerator FlashAndText(Image img, TextMeshProUGUI txt)
+    {
+        Color originalColor = img.color;
+        Color flashColor = new Color(originalColor.r, originalColor.g, originalColor.b, 1f);
+        img.color = flashColor;
+        txt.gameObject.SetActive(true);
+
+
+        yield return new WaitForSeconds(flashDuration);
+
+        flashColor.a = 0f; // Alpha设为0
+        img.color = flashColor;
+
+        yield return new WaitForSeconds(3-flashDuration);
+        txt.gameObject.SetActive(false);
+
+    }
+
+    private void RotateArrowTowards(Vector3 direction, RectTransform arrowTransform)
+    {
+        float angle = Vector2.SignedAngle(Vector2.up, direction);
+        arrowTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    Vector2 FindIntersection(Vector3 playerPos, Vector3 lightboxPos)
         {
             // Convert to screen coordinates
             Vector2 playerScreenPos = mainCamera.WorldToScreenPoint(playerPos);
@@ -107,9 +190,13 @@ public class SoundScreenEffect : MonoBehaviour
             if (IsPointOnScreen(playerScreenPos) && IsPointOnScreen(lightboxScreenPos))
             {
                 if (lightboxScreenPos.x > playerScreenPos.x)
-                    return new Vector2(Screen.width, lightboxScreenPos.y);
+                {
+                    return new Vector2(Screen.width - greenDotWidth / 2, lightboxScreenPos.y); // 向左偏移
+                }
                 else
-                    return new Vector2(0, lightboxScreenPos.y);
+                {
+                    return new Vector2(greenDotWidth / 2, lightboxScreenPos.y); // 向右偏移
+                }
             }
 
             // Calculate the intersection of the line segment with screen bounds
@@ -119,15 +206,28 @@ public class SoundScreenEffect : MonoBehaviour
             float screenTop = Screen.height;
 
             if (LineSegmentsIntersection(playerScreenPos, lightboxScreenPos, new Vector2(screenLeft, screenBottom), new Vector2(screenLeft, screenTop), out Vector2 intersectionLeft))
+            {
+                intersectionLeft.x += greenDotWidth / 2;  // 向右偏移
                 return intersectionLeft;
+            }
             if (LineSegmentsIntersection(playerScreenPos, lightboxScreenPos, new Vector2(screenRight, screenBottom), new Vector2(screenRight, screenTop), out Vector2 intersectionRight))
+            {
+                intersectionRight.x -= greenDotWidth / 2;  // 向左偏移
                 return intersectionRight;
+            }
             if (LineSegmentsIntersection(playerScreenPos, lightboxScreenPos, new Vector2(screenLeft, screenBottom), new Vector2(screenRight, screenBottom), out Vector2 intersectionBottom))
+            {
+                intersectionBottom.y += greenDotHeight / 2;  // 向上偏移
                 return intersectionBottom;
+            }
             if (LineSegmentsIntersection(playerScreenPos, lightboxScreenPos, new Vector2(screenLeft, screenTop), new Vector2(screenRight, screenTop), out Vector2 intersectionTop))
+            {
+                intersectionTop.y -= greenDotHeight / 2;  // 向下偏移
                 return intersectionTop;
+            }
 
-            return Vector2.zero;
+
+        return Vector2.zero;
         }
 
         bool IsPointOnScreen(Vector2 point)
