94e50090f62dbe816eba10eaaa46c7cd4cf4ce80
zzhcris <143578612+zzhcris@users.noreply.github.com>
2023-11-02 19:04:07 -0700

    Level2Update

---
+++ Assets/Level2 Script/Tground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tground : MonoBehaviour
+{
+    public float breakForceThreshold = 50f; // 破坏阈值
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > breakForceThreshold)
+        {
+            BreakWall();
+        }
+    }
+
+    void BreakWall()
+    {
+        // Add visual and audio effects for breaking the wall
+        Destroy(gameObject);
+    }
+}
